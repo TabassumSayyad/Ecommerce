fe208b4b83accf60bc3cf2a19314704a48251924
Tabassum <tabassumsayyad891@gmail.com>
2024-04-08 18:02:49 +0530

    Admin to admin

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -24,17 +24,17 @@ router.patch("/password/reset/:token",resetPassword);
 router.get("/me", isAuthenticatedUser, getuserDetails);
 router.put("/me/update",isAuthenticatedUser,updateProfile)
 //admin
-router.get("/admin/users", isAuthenticatedUser, authorizeRoles("Admin"), getAllUsers);
+router.get("/admin/users", isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
 router.get(
   "/admin/user/:id",
   isAuthenticatedUser,
-  authorizeRoles("Admin"),
+  authorizeRoles("admin"),
   getSingleUser
 );
 router.delete(
   "/admin/user/:id",
   isAuthenticatedUser,
-  authorizeRoles("Admin"),
+  authorizeRoles("admin"),
   deleteUser
 );
 module.exports = router;
