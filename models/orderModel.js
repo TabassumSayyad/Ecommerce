d898bb899c361f2fc697e35197b00d8d73c86982
Tabassum <tabassumsayyad891@gmail.com>
2024-04-15 18:32:24 +0530

    changes in schema

--- models/orderModel.js
+++ models/orderModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   shippingInfo: {
-    hounseNo:{
+    houseNo:{
         type: String,
         required: true,
     },
