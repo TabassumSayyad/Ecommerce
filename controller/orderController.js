383b600dbbb626bb0e03369d9f864a85bd4d64ca
Tabassum <tabassumsayyad891@gmail.com>
2024-04-18 11:02:17 +0530

    addded mail functionality during updating the order

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,5 +1,6 @@
 const Order = require("../models/orderModel");
 const Product = require("../models/productModel");
+const sendEmail = require("../utils/sendEmail");
 //create new Order
 exports.newOrder = async (req, res, next) => {
   const {
@@ -52,11 +53,9 @@ exports.newOrder = async (req, res, next) => {
   }
 };
 
-
 //get my orders(logged in users)
-exports.myOrders = async(req,res,next)=>
-{
-  const orders = await Order.find({ user: req.user._id })
+exports.myOrders = async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
   // .populate({
   //   path: "orderItems",
   //   populate: {
@@ -69,37 +68,42 @@ exports.myOrders = async(req,res,next)=>
     success: true,
     orders,
   });
-}
+};
 
 //get single order
-exports.getSingleOrder =async (req, res, next) => {
-  try{  const order = await Order.findById(req.params.id).populate("user", "name email")
-  // .populate({
-  //   path: "orderItems",
-  //   populate: {
-  //     path: "product",
-  //     select: "name price images",
-  //   },
-  // });
+exports.getSingleOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    // .populate({
+    //   path: "orderItems",
+    //   populate: {
+    //     path: "product",
+    //     select: "name price images",
+    //   },
+    // });
 
-  if (!order) {
-    return res.json({success:false,error:"Order not found with this Id"});
+    if (!order) {
+      return res.json({
+        success: false,
+        error: "Order not found with this Id",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (e) {
+    res.status(400).json({ success: false, error: e.message });
   }
-
-  res.status(200).json({
-    success: true,
-    order,
-  });
-}
-catch(e)
-{
-  res.status(400).json({success:false,error:e.message})
-}
 };
 
 //get All Orders(Admin)
-exports.getAllOrders =async (req, res, next) => {
-  const orders = await Order.find({isDeleted:false});
+exports.getAllOrders = async (req, res, next) => {
+  const orders = await Order.find({ isDeleted: false });
 
   res.status(200).json({
     success: true,
@@ -108,8 +112,8 @@ exports.getAllOrders =async (req, res, next) => {
 };
 
 //get deleted order(Admin)
-exports.getDeletedOrders =async (req, res, next) => {
-  const orders = await Order.find({isDeleted:true});
+exports.getDeletedOrders = async (req, res, next) => {
+  const orders = await Order.find({ isDeleted: true });
 
   res.status(200).json({
     success: true,
@@ -138,30 +142,100 @@ exports.deleteOrder = async (req, res) => {
 
 //update order (Admin)
 exports.updateOrder = async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
-
-  if (!order) {
-    return res.json({success:false,error:"Order not found with this Id"});
-  }
-
-  if (order.deliveryStatus === "Delivered") {
-    return res.json({success:false,error:"You have already delivered this order"});
-  }
-
-  order.deliveryStatus = req.body.deliveryStatus;
-
-  if (req.body.deliveryStatus === "Delivered") {
-    order.deliveredAt = Date.now();
-    if (order.paymentInfo.mode === "COD")
-    {
-      order.paidAt=Date.now();
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    if (!order) {
+      return res.json({
+        success: false,
+        error: "Order not found with this Id",
+      });
     }
-  }
+    console.log(order.user.email);
+    if (order.deliveryStatus === "Delivered") {
+      return res.json({
+        success: false,
+        error: "You have already delivered this order",
+      });
+    }
+    let message;
 
-  await order.save({ validateBeforeSave: false });
-  res.status(200).json({
-    success: true,
-  });
+    if (req.body.deliveryStatus === "Shipped") {
+      message = `
+      <html>
+      <head>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h2>Order Status</h2>
+          </div>
+          <p>Hello ${order.user.name},</p>
+          <p>We are thrilled to inform you that your recent order has been successfully Shipped!</p>
+          <p>Your eagerly anticipated order is en route and scheduled to arrive at your doorstep within the next 24 hours.</p>
+          <div class="footer">
+            <p>Thank you,</p>
+            <p>The Kharido Yaar Team</p>
+          </div>
+        </div>
+      </body>
+    </html>
+    `;
+    } else if (req.body.deliveryStatus === "Delivered") {
+      message = `
+      <html>
+      <head>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h2>Order Status</h2>
+          </div>
+          <p>Hello ${order.user.name},</p>
+          <p>We are pleased to inform you that your recent order has been successfully Delivered!</p>
+          <p>Thank you for choosing Kharido Yaar.</p>
+          <div class="footer">
+            <p>Thank you,</p>
+            <p>The Kharido Yaar Team</p>
+          </div>
+        </div>
+      </body>
+    </html>
+    `;
+    }
+
+    order.deliveryStatus = req.body.deliveryStatus;
+    if (req.body.deliveryStatus === "Shipped") {
+      await sendEmail({
+        email: order.user.email,
+        subject: `Order Delivery Status`,
+        message,
+        contentType: "text/html", // Set the content type to HTML
+      });
+    }
+
+    if (req.body.deliveryStatus === "Delivered") {
+      order.deliveredAt = Date.now();
+      if (order.paymentInfo.mode === "COD") {
+        order.paidAt = Date.now();
+      }
+      await sendEmail({
+        email: order.user.email,
+        subject: `Order Delivery Status`,
+        message,
+        contentType: "text/html", // Set the content type to HTML
+      });
+    }
+
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+    });
+  } catch (e) {
+    res.status(400).json({ success: false, e });
+  }
 };
 
 async function updateStock(id, quantity) {
