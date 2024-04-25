52dbeb3574fa4bc55bbb598ce348907e1ad0b59b
Tabassum <tabassumsayyad891@gmail.com>
2024-04-25 11:28:23 +0530

    updated server.js

--- server.js
+++ server.js
@@ -2,38 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const userRoute = require("./routers/userRoute");
-const productRoute=require("./routers/productRoute");
-const contactRoute=require("./routers/contactRoute");
-const categoryRoute= require("./routers/categoryRoute")
-const orderRoute=require("./routers/orderRoute")
-const paymentRoute=require("./routers/paymentRoute")
-const cookieParser=require('cookie-parser')
-const path = require("path")
-require('dotenv').config({path:path.resolve(__dirname,"./config/config.env")})
-const dbConnection= require("./db/conn")
+const productRoute = require("./routers/productRoute");
+const contactRoute = require("./routers/contactRoute");
+const categoryRoute = require("./routers/categoryRoute");
+const orderRoute = require("./routers/orderRoute");
+const paymentRoute = require("./routers/paymentRoute");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, "./config/config.env"),
+});
+const dbConnection = require("./db/conn");
+const { config } = require("dotenv");
 dbConnection();
 
-// console.log(process.env.PORT)
 const port = process.env.PORT || 8000;
 
-console.log(__dirname)
+app.use("/uploads/images", express.static("uploads/images"));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-app.use("/uploads/images",express.static("uploads/images"))
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v1",userRoute);
-app.use("/api/v1",productRoute);
-app.use("/api/v1",contactRoute);
-app.use("/api/v1",categoryRoute);
-app.use("/api/v1",orderRoute)
-app.use("/api/v1",paymentRoute)
-app.listen(port, () => {
-    console.log(`Connection is setup at port ${port}`);
-  });
+app.use("/api/v1", userRoute);
+app.use("/api/v1", productRoute);
+app.use("/api/v1", contactRoute);
+app.use("/api/v1", categoryRoute);
+app.use("/api/v1", orderRoute);
+app.use("/api/v1", paymentRoute);
 
+app.listen(port, () => {
+  console.log(`Connection is setup at port ${port}`);
+});
