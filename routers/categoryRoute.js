e5cc8e657b941884c6e1f3fe59c1802c725a4ab1
Tabassum <tabassumsayyad891@gmail.com>
2024-04-25 11:00:26 +0530

    added authentication and authorization part

--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const{createCategory,getAllCategories}=require('../controller/categoryController')
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.post("/category",createCategory)
-router.get("/category",getAllCategories)
+router.post("/category",isAuthenticatedUser,authorizeRoles("admin"),createCategory)
+router.get("/category",isAuthenticatedUser,getAllCategories)
 
 module.exports = router;
